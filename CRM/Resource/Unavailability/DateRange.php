fec0e9298df45a4c8538e2d745392a883ce10f15
B. Endres <endres@systopia.de>
2021-09-30 21:26:21 +0200

    detailed unavailabilities

--- CRM/Resource/Unavailability/DateRange.php
+++ CRM/Resource/Unavailability/DateRange.php
@@ -15,28 +15,10 @@
 
 use CRM_Resource_ExtensionUtil as E;
 
-class CRM_Resource_BAO_ResourceAssignment extends CRM_Resource_DAO_ResourceAssignment
+/**
+ * This unavailability defined by a date range
+ */
+class CRM_Resource_Unavailability_DateRange extends CRM_Resource_BAO_ResourceUnavailability
 {
-
-    /**
-     * Create a new ResourceAssignment based on array-data
-     *
-     * @param array $params key-value pairs
-     *
-     * @return CRM_Resource_DAO_ResourceAssignment|NULL
-     *
-     * public static function create($params) {
-     * $className = 'CRM_Resource_DAO_ResourceAssignment';
-     * $entityName = 'ResourceAssignment';
-     * $hook = empty($params['id']) ? 'create' : 'edit';
-     *
-     * CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-     * $instance = new $className();
-     * $instance->copyValues($params);
-     * $instance->save();
-     * CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
-     *
-     * return $instance;
-     * } */
-
+    // todo: implement
 }
