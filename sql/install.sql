dcc8fc20a9d61ac8ba4fc788e686b1d14981eca7
B. Endres <endres@systopia.de>
2021-10-05 12:18:58 +0200

    update schema - added unavailability reason

--- sql/install.sql
+++ sql/install.sql
@@ -51,6 +51,7 @@ CREATE TABLE IF NOT EXISTS  `civicrm_resource_demand_condition` (
 
 CREATE TABLE IF NOT EXISTS  `civicrm_resource_unavailability` (
    `id`                int unsigned   NOT NULL AUTO_INCREMENT  COMMENT 'Unique Resource Unavailability ID',
+   `reason`            varchar(255)                            COMMENT 'Reason for the unavailability',
    `resource_id`       int unsigned   NOT NULL                 COMMENT 'Resource Demand ID',
    `class_name`        varchar(127)                            COMMENT 'Class name of the implementation, a subclass of CRM_Resource_BAO_Resource_Unavailability',
    `parameters`        varchar(255)                            COMMENT 'A json encoded data blob to store the parameters of this specific unavailability',
